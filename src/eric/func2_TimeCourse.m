e204cf5acf988ae2ca75c9eb4341d6f6e52aa393
Jamie Morton <jamietmorton@gmail.com>
2014-12-05 13:37:11 -0700

    Adding simulated annealing code

--- src/eric/func2_TimeCourse.m
+++ src/eric/func2_TimeCourse.m
@@ -8,45 +8,49 @@ function [time, y_vals, params, conc, conc2]=func2_TimeCourse(params, ...
                                                               conc, ...
                                                               conc2)
 
-%global p_mine conc conc2
-%p_mine=params;
-EGF_cell=0;
+    %global p_mine conc conc2
+    %p_mine=params;
+    EGF_cell=0;
 
-y=zeros(2,length(initial_conditions));
-y(1,:)=initial_conditions;
-%time=[];y_all=[];
-time = zeros(1,numel(time_course)-1);
-y_all = zeros(1,numel(time_course)-1);
+    y=zeros(numel(time_course)-1,length(initial_conditions)); %species concentrations over time
 
-for t=1:numel(time_course)-1
-    if time_course(t)==tp
-        %y(t,:)=y(t,:)+pert;
-    end
-    %time of egf addition
-    if time_course(t)>te
-        EGF_cell=EGF_conc;
-    end
-    %time of perturbation
-    if time_course(t)>tp
-        %parameters change upon inhibitor addition
-        conc2(inhib(1))=conc(inhib(1),inhib(2));
-    end
-    
-    options=odeset('NonNegative',1:25);
-    [time_now, y_now]=ode45(@func_ODEs,t:t+1,y(t,:),conc2,params,options);
-    %time=[time;time_now];
-    %y_all=[y_all;y_now];
-    time(t) = time_now;
-    y_all(t) = y_now;
-    y_cor=y_all;
-    %y_cor(y_cor<0)=0;
-    %%Where the heck does 17 come from?
-    if y_cor(end,17)<EGF_cell,y_cor(end,17)=EGF_cell;end
-    
-    y(t+1,:)=y_cor(end,:);
-end
+    y(1,:)=initial_conditions;
+    %time=[];y_all=[];
+    time = zeros(1,numel(time_course)-1);
+    y_all = zeros(numel(time_course)-1,length(initial_conditions));
 
-y_vals=y;
+    for t=1:numel(time_course)-1
+        %if time_course(t)==tp
+            %y(t,:)=y(t,:)+pert;
+        %end
+        %time of egf addition
+        if time_course(t)>te
+            EGF_cell=EGF_conc;
+        end
+        %time of perturbation
+        if time_course(t)>tp
+            %parameters change upon inhibitor addition
+            conc2(inhib(1))=conc(inhib(1),inhib(2));
+        end
+
+        options=odeset('NonNegative',1:25);
+        %[time_now, y_now]=ode45(@func_ODEs,t:t+1,y(t,:),conc2,params,options);
+        [time_now, y_now]=ode45(@(T,Y) func_ODEs(T,Y,conc2,params),t:t+1,y(t,:),options);
+        %time=[time;time_now];
+        %y_all=[y_all;y_now];
+        time(t) = time_now(1);
+        y_all(t,:) = y_now(1,:);
+        y_cor=y_now;
+        %y_cor(y_cor<0)=0;
+        %% 17 = 17th species. EGFl
+        if y_cor(end,17)<EGF_cell
+            y_cor(end,17)=EGF_cell;
+        end %correction
+
+        y(t+1,:)=y_cor(end,:);
+    end
+
+    y_vals=y;
 
 end
 
---
+++ src/main/mcmc_tune.m
@@ -0,0 +1,90 @@
+%MCMC
+clear,clc
+addpath ../eric
+
+k = 1.38e-23; %Boltzmann constant
+
+%Load model values
+numWalkers = 10;
+numIterations = 10;
+numParams = 56;
+params = zeros(numWalkers,numParams);
+best_params = zeros(numWalkers,numParams);
+scores = zeros(numWalkers);
+params(1,:) = xlsread('../../params/simplified values.xlsx','Values2','I2:I57');
+
+conc = zeros(numWalkers,6,10);
+%global conc conc2
+conc(1,1,:)=xlsread('../../params/Inhib Values.xlsx','0hr','B2:K2');
+conc(1,2,:)=xlsread('../../params/Inhib Values.xlsx','0hr','M2:V2');
+conc(1,3,:)=xlsread('../../params/Inhib Values.xlsx','0hr','X2:AG2');
+conc(1,4,:)=xlsread('../../params/Inhib Values.xlsx','0hr','AI2:AR2');
+conc(1,5,:)=xlsread('../../params/Inhib Values.xlsx','0hr','AT2:BC2');
+conc(1,6,:)=xlsread('../../params/Inhib Values.xlsx','0hr','BE2:BN2');
+conc2 = zeros(numWalkers,6);
+equ_fract=.21;
+egf_fract=.48;
+
+%%Create copies for each walker
+for walker = 2:numWalkers
+    params(walker,:) = params(1,:);
+    conc(walker,:,:) = conc(1,:,:);
+    conc2(walker,:)= squeeze(conc(walker,:,1));
+end
+initial_conditions=xlsread('../../params/simplified values.xlsx','Values2','E2:E26');
+%conc2=conc(:,1);
+ERKt=initial_conditions(11)+initial_conditions(12);
+
+opts=optimset('Display','iter','MaxFunEvals',1e3,'MaxIter',5e2);
+fract=[equ_fract; egf_fract];
+EGF_conc=[0; 100];%in Molar
+tf=[600 700 800];
+inhib=[1,1];
+conc2(inhib(1))=inhib(2);
+
+time_course = 0:801;
+tp=1;te=500;
+%r2 = func3_fmin(params,initial_conditions,EGF_conc,inhib,time_course,te,tp,fract,tf,conc, conc2);
+%res=fminsearch(@(params) func3_fmin(params,initial_conditions,EGF_conc,inhib,time_course,te,tp,fract,tf),params,opts);
+
+%%Run Monte Carlo Markov Chain algorithm (Metropolis Hastings algorithm)
+%%http://en.wikipedia.org/wiki/Metropolis%E2%80%93Hastings_algorithm
+%%Also known as simulated annealing
+parfor walker = 1:numWalkers
+    %Now generate a random walk
+    walker
+    c = squeeze(conc(walker,:,:));
+    c2 = squeeze(conc2(walker,:));
+    [p, r2] = simulannealbnd(@(p) func3_fmin(p,initial_conditions,EGF_conc,inhib,time_course,te,tp,fract,tf,c,c2), ...
+                                  params(walker,:),0,Inf);
+    scores(walker) = r2;
+    best_params(walker,:) = p;
+    
+%     c_ = c; c2_ = c2; %temporary variables for c and c2;
+%     [prev_r2,~,c_,c2_] = func3_fmin(params(walker,:),initial_conditions,EGF_conc,inhib,time_course,te,tp,fract,tf,c,c2);
+%     for i = 2:numIterations
+%         p = abs(normrnd(params(walker,:),ones(1,numParams)));
+%         [r2,~,c_,c2_] = func3_fmin(p,initial_conditions,EGF_conc,inhib,time_course,te,tp,fract,tf,c,c2);
+%         alpha = exp(r2-prev_r2/(k*T));
+%         if alpha>=1
+%             params(walker,:) = p;
+%             scores(walker) = r2;
+%             best_params(walker,:) = p;
+%             prev_r2 = r2;
+%             c = c_; c2 = c2_;
+%         else
+%             if rand<alpha
+%                 params(walker,:) = p;
+%                 c = c_; c2 = c2_;
+%             end
+%         end
+% 
+%     end
+end
+
+
+
+
+
+
+
